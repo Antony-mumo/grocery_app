c8c18b725d311da217b4a9cba901aa5c31c085a5
Levanio <55873520+Antony-mumo@users.noreply.github.com>
2021-09-13 13:41:54 +0300

    update.js

---
+++ build/precache-manifest.59778963b4000a5b78a89b94fc286f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d80a44d06b9687137f9578b092b502d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5249ad64f008e64cfd53",
+    "url": "/static/js/2.4404206f.chunk.js"
+  },
+  {
+    "revision": "2a3a23bf878d7187dc1e20049a44e9ba",
+    "url": "/static/js/2.4404206f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b5f7619e945dd06079",
+    "url": "/static/js/main.a44214ea.chunk.js"
+  },
+  {
+    "revision": "8c0eaf94f08a1c7cda32",
+    "url": "/static/js/runtime-main.867f0435.js"
+  },
+  {
+    "revision": "9b530d1cce317ba0653fbd89adb203a6",
+    "url": "/static/media/logo.9b530d1c.png"
+  }
+]);
